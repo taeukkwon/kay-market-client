67c3297551c63161f283198ae8a7f0a2c4bf83c7
 권태욱 <kwontaeuk@gwontaeug-ui-MacBook-Pro.local>
2021-04-18 12:34:43 +0900

    래리 API 훔치기

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://kay-market-server.herokuapp.com"
+    ? "https://larry-market-server.herokuapp.com"
     : "http://localhost:8080";
